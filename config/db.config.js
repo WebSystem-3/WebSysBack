000675a2ac2a3c3daee305d668c23d86b7f1ef8e
HongTorxh <ghdtack@ajou.ac.kr>
2023-11-10 16:10:44 +0900

    user 관련 method 작성

---
+++ config/db.config.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+const { connect } = require("../routes");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "12345678", // 본인에 해당하는 password로 수정
+  database: "websys",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Mysql 연결 오류" + err);
+  } else {
+    console.log("Mysql 연결 성공");
+  }
+});
+module.exports = connection;
---
+++ routes/user.js
@@ -0,0 +1,77 @@
+var express = require("express");
+var router = express.Router();
+const db = require("../config/db.config.js");
+
+// 유저 정보 조회 (GET)
+router.get("/info/:id", (req, res, next) => {
+  const sql = "select * from users where user_id=" + req.params.id;
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    res.json(results);
+  });
+});
+
+//회원 가입
+router.post("/signup", (req, res, next) => {
+  const sql = "insert into users (account, password, name) values (?, ?, ?)";
+  const params = [req.body.account, req.body.password, req.body.name];
+  db.query(sql, params, (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    res.json(results);
+  });
+});
+
+// 존재하는 id인지 여부 확인
+router.post("/validation", (req, res, next) => {
+  const sql = "select account from users where account=?";
+  const params = [req.body.account];
+  db.query(sql, params, (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      return res.status(500).json({ message: "이미 사용중인 계정입니다." });
+    } else {
+      return res.status(200).json({ message: "사용 가능한 계정입니다." });
+    }
+  });
+});
+
+// 회원정보 수정 (patch)
+router.patch("/edit/:id", (req, res, next) => {
+  let sql = "select * from users where user_id=" + req.params.id;
+  const body = [req.body.password, req.body.name];
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    if (results.length === 0) {
+      return res.status(500).json({ message: "계정이 존재하지 않습니다." });
+    } else {
+      let sql =
+        "update users set password = ?,name = ? where user_id=" + req.params.id;
+      db.query(sql, body, (err, results) => {
+        if (err) throw err;
+        return res
+          .status(200)
+          .json({ message: "사용자 정보가 변경되었습니다." });
+      });
+    }
+  });
+});
+
+// 회원 탈퇴
+router.delete("/delete/:id", (req, res, next) => {
+  let sql = "select  * from users where user_id=" + req.params.id;
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    if (results.length === 0) {
+      return res.status(500).json({ message: "계정이 존재하지 않습니다." });
+    } else {
+      let sql = "delete from users where user_id=" + req.params.id;
+      db.query(sql, (err, results) => {
+        if (err) throw err;
+        return res.status(200).json({ message: "성공적으로 삭제되었습니다." });
+      });
+    }
+  });
+});
+module.exports = router;
