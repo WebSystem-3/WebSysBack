96798722163668ff3159f687548af2ffad3acdf7
seoyeomi <seoyeonii@ajou.ac.kr>
2023-12-07 11:37:01 +0900

    friend controller message 수정

--- controller/FriendController.js
+++ controller/FriendController.js
@@ -5,7 +5,7 @@ module.exports = {
     const { user_id1 } = req.params;
     if (!user_id1) {
       return res.status(400).json({
-        message: "user_id1이 존재하지 않습니다.",
+        message: "존재하지 않는 유저 아이디입니다.",
       });
     }
     try {
@@ -26,7 +26,7 @@ module.exports = {
       const result = await FriendService.findFriendsByAccount(account);
       if (!result) {
         return res.status(400).json({
-          message: "해당 유저가 존재하지 않습니다.",
+          message: "존재하지 않는 유저입니다.",
         });
       }
       return res.status(200).json(result);
@@ -38,19 +38,19 @@ module.exports = {
     const { user_id1, user_id2 } = req.params;
     if (!user_id1) {
       return res.status(400).json({
-        message: "user_id1이 존재하지 않습니다.",
+        message: "존재하지 않는 유저 아이디입니다.",
       });
     }
     if (!user_id2) {
       return res.status(400).json({
-        message: "user_id2가 존재하지 않습니다.",
+        message: "존재하지 않는 유저입니다.",
       });
     }
     try {
       const friend = await FriendService.isValidUserId2(user_id2);
       if (!friend) {
         return res.status(400).json({
-          message: "user_id2를 입력해주세요.",
+          message: "유저 아이디를 입력해주세요.",
         });
       }
       const result = FriendService.createFriend(user_id1, user_id2);
@@ -67,12 +67,12 @@ module.exports = {
     const { user_id1, user_id2 } = req.params;
     if (!user_id1) {
       return res.status(400).json({
-        message: "user_id1이 존재하지 않습니다.",
+        message: "존재하지 않는 유저 아이디입니다.",
       });
     }
     if (!user_id2) {
       return res.status(400).json({
-        message: "user_id2가 존재하지 않습니다.",
+        message: "존재하지 않는 유저입니다.",
       });
     }
     try {
@@ -94,12 +94,12 @@ module.exports = {
     const { user_id1, user_id2 } = req.params;
     if (!user_id1) {
       return res.status(400).json({
-        message: "user_id1이 존재하지 않습니다.",
+        message: "존재하지 않는 유저 아이디입니다.",
       });
     }
     if (!user_id2) {
       return res.status(400).json({
-        message: "user_id2가 존재하지 않습니다.",
+        message: "존재하지 않는 유저입니다.",
       });
     }
     try {
@@ -109,7 +109,7 @@ module.exports = {
       );
       if (!friend) {
         return res.status(400).json({
-          message: "친구가 존재하지 않습니다.",
+          message: "존재하지 않는 친구입니다.",
         });
       }
       const result = await FriendService.getFriendTask(user_id2);
