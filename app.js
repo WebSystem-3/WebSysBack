9cecc270d266208fcbf767ebbf680a25753fe388
seoyeomi <seoyeonii@ajou.ac.kr>
2023-12-08 11:39:49 +0900

    express-session

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const cors = require("cors");
-const session = require("session");
+const session = require("express-session");
 
 var indexRouter = require("./routes/index");
 
