33917ed18b4855b8f4eae3a17b284b3d6af2508e
HongTorxh <ghdtack@ajou.ac.kr>
2023-12-10 20:58:05 +0900

    최종 전

--- models/FriendModel.js
+++ models/FriendModel.js
@@ -5,7 +5,7 @@ module.exports = {
     "SELECT * FROM friends WHERE user_id1=? AND user_id2=?",
   createFriend: "INSERT INTO friends (user_id1, user_id2) VALUES (?, ?)",
   deleteFriendByUserId1: "DELETE FROM friends WHERE user_id1=?",
-  deleteFriendByUserId2: "DELETE FROM friends WHERE user_id=?",
+  deleteFriendByUserId2: "DELETE FROM friends WHERE user_id2=?",
   deleteFriendByUserId1AndUserId2:
     "DELETE FROM friends WHERE user_id1=? AND user_id2=?",
 };
