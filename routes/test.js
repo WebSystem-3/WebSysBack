8af77ec1e337970aa12d7ff3f5f631b527584009
HongTorxh <ghdtack@ajou.ac.kr>
2023-12-10 20:54:52 +0900

    최종 전

--- routes/test.js
+++
@@ -1,8 +0,0 @@
-// routes/test.js
-const express = require("express");
-const router = express.Router();
-const UserController = require("../controller/UserController");
-
-router.post("/test-login", UserController.testLogin);
-
-module.exports = router;
